1262664a2f08d6376c87e3ce8755140e45a5dd2d
Anandagadugin <gaduginanand984@gmail.com>
2023-03-29 21:04:23 +0530

    Added few expenses items

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense item</h2>
+      <div>Food Rs 10</div>
+      <div>
+        <div>Petrol Rs 100</div>
+      </div>
+      <div>Movies Rs 200</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
