d419f09e1d285a2d7c8cc16c20dc9e6e76faaff1
Anandagadugin <gaduginanand984@gmail.com>
2023-07-15 23:03:01 +0530

    Rendering Expenses with performance optimisations

--- src/App.js
+++ src/App.js
@@ -2,51 +2,54 @@ import React, { useState } from "react";
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 import NewExpenses from "./components/NewExpenses/NewExpenses";
 
-const App = () => {
-  const [expenses, setExpenses] = useState([
-    {
-      id: "e1",
-      title: "Bike insurance",
-      amount: 54.12,
-      date: new Date(2020, 4, 12),
-      location: "BMT insurance Pvt Ltd",
-    },
-    {
-      id: "e2",
-      title: "Home loan",
-      amount: 749.49,
-      date: new Date(2021, 2, 6),
-      location: "HR layout bangalore",
-    },
-    {
-      id: "e3",
-      title: "Power bill",
-      amount: 453.12,
-      date: new Date(2021, 2, 28),
-      location: "BESCOM bill centre",
-    },
-    {
-      id: "e4",
-      title: "Wifi charges",
-      amount: 345,
-      date: new Date(2021, 5, 23),
-      location: "BSNL internet centre",
-    },
-  ]);
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Bike insurance",
+    amount: 54.12,
+    date: new Date(2020, 4, 12),
+    location: "BMT insurance Pvt Ltd",
+  },
+  {
+    id: "e2",
+    title: "Home loan",
+    amount: 749.49,
+    date: new Date(2021, 2, 6),
+    location: "HR layout bangalore",
+  },
+  {
+    id: "e3",
+    title: "Power bill",
+    amount: 453.12,
+    date: new Date(2021, 2, 28),
+    location: "BESCOM bill centre",
+  },
+  {
+    id: "e4",
+    title: "Wifi charges",
+    amount: 345,
+    date: new Date(2021, 5, 23),
+    location: "BSNL internet centre",
+  },
+];
 
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((preExpenses) => {
+      return [expense, ...preExpenses];
+    });
+  };
+  
   const onDeleteExpense = (expenseId) => {
     setExpenses((prevExpenses) =>
       prevExpenses.filter((expense) => expense.id !== expenseId)
     );
   };
-
-  const addExpenseHandler = (expese) => {
-    console.log("in App.js");
-    console.log(expese);
-  };
   return (
     <div>
-      <NewExpenses onAddExpese ={addExpenseHandler}/>
+      <NewExpenses onAddExpese={addExpenseHandler} />
       {expenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -40,7 +40,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amout: enteredAmount,
+      amount: enteredAmount,
       date: new Date(enteredDate),
     };
     props.onSaveExpenseData(expenseData);
